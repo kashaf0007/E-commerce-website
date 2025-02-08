0a0aadcc06d7e5ad3ff5de62cd5c03a70c5a7891
kashaf Aman <you@example.com>
2025-02-08 23:36:20 +0500

    updateed

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -5,17 +5,17 @@ import { RxCross2 } from "react-icons/rx";
 const Header = () => {
   return (
     <header className='bg-white shadow-sm border-b mx-auto flex flex-col w-1440px'>
-    <div className='bg-black text-white py-2  text-sm flex justify-between items-center px-8 w-full'>
+      <div className='bg-black text-white py-2  text-sm flex justify-between items-center px-8 w-full'>
         <div className='text-center flex-1 '>
-    Sign up and get 20% off to your first order.
-<a href='#' className='underline hover-text-slate-300' > Sign Up Now </a>
-    </div>
+          Sign up and get 20% off to your first order.
+          <a href='#' className='underline hover-text-slate-300' > Sign Up Now </a>
+        </div>
         <div className='text-white text-xl '>
-        <RxCross2 />
+          <RxCross2 />
         </div>
-        
-        </div>
-        </header>
+
+      </div>
+    </header>
   )
 }
 
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -3,16 +3,16 @@ import { FaCartShopping } from "react-icons/fa6";
 import Link from 'next/link'
 
 const Navbar = () => {
-    return (
-      
-  
+  return (
+
+
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center bg-gray-300">
       <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
-     <Image
-      src="/logo.jpeg" 
-      alt="logo"
-      height={30} 
-      width={100} />
+        <Image
+          src="/logo.jpeg"
+          alt="logo"
+          height={30}
+          width={100} />
         <span className="ml-3 text-2xl font-bold ">SHOPPING-WEBSITE</span>
       </a>
       <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base  justify-center ">
@@ -20,14 +20,14 @@ const Navbar = () => {
         <Link href="/" className="mr-5  text-black hover:text-gray-500 font-bold text-2xl">Products</Link>
         <Link href="/About" className="mr-5  text-black hover:text-gray-500 font-bold text-2xl">About</Link>
         <Link href="/Contact" className="mr-5  text-black hover:text-gray-500 font-bold text-2xl">Contact</Link>
-   </nav>
-   <div className="px-4" >
-   <Link href="/Cart" className="relative">
+      </nav>
+      <div className="px-4" >
+        <Link href="/Cart" className="relative">
 
-<FaCartShopping  className="text-4xl text-black hover:text-gray-400" />
-</Link>
-   </div>
-   </div>
-    )
+          <FaCartShopping className="text-4xl text-black hover:text-gray-400" />
+        </Link>
+      </div>
+    </div>
+  )
 }
 export default Navbar
